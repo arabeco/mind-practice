08746f60a5ec7abd032aa8ef6fbce100a61f605b
misterxhermit <misterxhermit@gmail.com>
2026-04-21 07:44:07 -0300

    feat(bayes): add barrel export

---
+++ src/lib/bayesEngine/index.ts
@@ -0,0 +1,7 @@
+// src/lib/bayesEngine/index.ts
+export * from './types';
+export * from './belief';
+export * from './likelihood';
+export * from './drift';
+export * from './update';
+export * from './archetype';
